1673485e5cf66a4ba7a08508bd2e8bb2dda62b76
lisa427 <lisa.mcgowan4@gmail.com>
2021-01-17 23:39:42 -0500

    map has markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,1 +1,61 @@
-// code
+// store url to retrieve earthquake data
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// send request for the json data
+d3.json(queryUrl, function(data) {
+    createFeatures(data.features);
+});
+
+function createFeatures(eqData) {
+
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.mag +
+          "</h3>");
+      }
+
+    let earthquakes = L.geoJSON(eqData, {
+        onEachFeature: onEachFeature
+      });
+
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+    // define base layer
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/light-v10",
+      accessToken: API_KEY
+    });
+    
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+      "Light Map": lightmap,
+    };
+  
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    var myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [lightmap, earthquakes]
+    });
+}
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+//     L.control.layers(baseMaps, overlayMaps, {
+//       collapsed: false
+//     }).addTo(myMap);
+//   }
+  
